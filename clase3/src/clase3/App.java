01bbaf9c9105d7233a34653309e6174da887e189
Mancaceresuade <mancaceres@uade.edu.ar>
2025-03-27 11:59:57 -0300

    clase 3-inicio

---
+++ clase3/src/clase3/App.java
@@ -0,0 +1,139 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Conjunto<Integer> c1 = new Conjunto<Integer>(5);
+        c1.agregar(2);
+        c1.agregar(3);
+        c1.agregar(6);
+        c1.mostrar();
+        ConjuntoDinamico<Integer> c2 = new ConjuntoDinamico<Integer>();
+        c2.agregar(10);
+        c2.agregar(20);
+        c2.agregar(5);       
+        c2.mostrar();
+
+    }
+}
+
+
+class Conjunto<T> {
+    T[] elementos;
+    int tam;
+    Conjunto(int tam) {
+        this.tam = tam;
+        elementos = (T[]) new Object[tam];        
+    }
+    public void agregar(T elemento) {
+        for (int i = 0; i < tam; i++) {
+            if (elementos[i] == null) {
+                elementos[i] = elemento;
+                break;
+            }
+        }
+    }
+    public boolean pertenece(T elemento) {
+        for (int i = 0; i < tam; i++) {
+            if (elementos[i] == elemento) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean esVacio() {
+        for (int i = 0; i < tam; i++) {
+            if (elementos[i] != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public int cardinalidad() {
+        int cont = 0;
+        for (int i = 0; i < tam; i++) {
+            if (elementos[i] != null) {
+                cont++;
+            }
+        }
+        return cont;
+    }
+    public Conjunto<T> union(Conjunto<T> otro) {
+        Conjunto<T> nuevo = new Conjunto<T>(tam + otro.tam);
+        for (int i = 0; i < tam; i++) {
+            nuevo.agregar(elementos[i]);
+        }
+        for (int i = 0; i < otro.tam; i++) {
+            nuevo.agregar(otro.elementos[i]);
+        }
+        return nuevo;
+    }
+    public Conjunto<T> interseccion(Conjunto<T> otro) {
+        Conjunto<T> nuevo = new Conjunto<T>(tam);
+        for (int i = 0; i < tam; i++) {
+            if (otro.pertenece(elementos[i])) {
+                nuevo.agregar(elementos[i]);
+            }
+        }
+        return nuevo;
+    }
+    public Conjunto<T> diferencia(Conjunto<T> otro) {
+        Conjunto<T> nuevo = new Conjunto<T>(tam);
+        for (int i = 0; i < tam; i++) {
+            if (!otro.pertenece(elementos[i])) {
+                nuevo.agregar(elementos[i]);
+            }
+        }
+        return nuevo;
+    }
+    public boolean esSubconjunto(Conjunto<T> otro) {
+        for (int i = 0; i < tam; i++) {
+            if (!otro.pertenece(elementos[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public void mostrar() {
+        for (int i = 0; i < tam; i++) {
+            if (elementos[i] != null) {
+                System.out.println(elementos[i]);
+            }
+        }
+    }
+}
+
+
+class ConjuntoDinamico<T> {
+    
+    Nodo<T> primero;    
+    ConjuntoDinamico() {
+        this.primero = null;
+    }
+    public void agregar(T elemento) {
+        Nodo<T> nuevo = new Nodo<T>(elemento);
+        if (primero == null) {
+            primero = nuevo;
+        } else {
+            Nodo<T> actual = primero;
+            while (actual.siguiente != null) {
+                actual = actual.siguiente;
+            }
+            actual.siguiente = nuevo;
+        }
+    }
+    public void mostrar() {
+        Nodo<T> actual = primero;
+        while (actual != null) {
+            System.out.println(actual.elemento);
+            actual = actual.siguiente;
+        }
+    }
+}
+
+
+class Nodo<T> {
+    T elemento;
+    Nodo<T> siguiente;
+    Nodo(T elemento) {
+        this.elemento = elemento;
+        this.siguiente = null;
+    }
+}
---
+++ clase3/src/clase4/App.java
@@ -0,0 +1,150 @@
+package clase4;
+
+public class App {
+    public static void main(String[] args) {
+        Lista<String> palabras = new Lista<>(10);
+        palabras.agregar("Hola");
+        palabras.agregar("Mundo");
+        palabras.agregar("Java");
+        palabras.imprimirLista();
+
+        ListaDinamica<String> palabrasDinamica = new ListaDinamica<>();
+        palabras.agregar("Hola");
+        palabras.agregar("Mundo");
+        palabras.agregar("Java");
+        palabras.imprimirLista();
+
+
+    }
+    
+}
+
+class Lista<T> {
+    private T[] elementos;
+    private int contador = 0;
+
+    public Lista(int capacidad) {
+        elementos = (T[]) new Object[capacidad];
+    }
+
+    public void insertar(int pos, T elemento) {
+        for (int i = contador; i > pos; i--) {
+            elementos[i] = elementos[i - 1];
+        }
+        elementos[pos] = elemento;
+        contador++;
+    }
+
+    public void agregar(T elemento) {
+        elementos[contador++] = elemento;
+    }
+
+    public T obtener(int indice) {
+        return elementos[indice];
+    }
+
+    public void imprimirLista() {
+        for (int i = 0; i < contador; i++) {
+            System.out.println(elementos[i]);
+        }
+    }
+
+}
+
+
+class ListaDinamica<T> {
+
+    private Nodo<T> primero;
+    private Nodo<T> ultimo;
+    private int contador = 0;
+
+    public void agregar(T elemento) {
+        Nodo<T> nuevo = new Nodo<>(elemento);
+        if (primero == null) {
+            primero = nuevo;
+            ultimo = nuevo;
+        } else {
+            ultimo.setSiguiente(nuevo);
+            ultimo = nuevo;
+        }
+        contador++;
+    }
+
+    public void imprimirLista() {
+        Nodo<T> actual = primero;
+        while (actual != null) {
+            System.out.println(actual.getElemento());
+            actual = actual.getSiguiente();
+        }
+    }
+
+    public T obtener(int indice) {
+        Nodo<T> actual = primero;
+        for (int i = 0; i < indice; i++) {
+            actual = actual.getSiguiente();
+        }
+        return actual.getElemento();
+    }
+
+    public void insertar(int pos, T elemento) {
+        Nodo<T> nuevo = new Nodo<>(elemento);
+        if (pos == 0) {
+            nuevo.setSiguiente(primero);
+            primero = nuevo;
+        } else {
+            Nodo<T> actual = primero;
+            for (int i = 0; i < pos - 1; i++) {
+                actual = actual.getSiguiente();
+            }
+            nuevo.setSiguiente(actual.getSiguiente());
+            actual.setSiguiente(nuevo);
+        }
+        contador++;
+    }
+
+    public void eliminar(int pos) {
+        if (pos == 0) {
+            primero = primero.getSiguiente();
+        } else {
+            Nodo<T> actual = primero;
+            for (int i = 0; i < pos - 1; i++) {
+                actual = actual.getSiguiente();
+            }
+            actual.setSiguiente(actual.getSiguiente().getSiguiente());
+            if (pos == contador - 1) {
+                ultimo = actual;
+            }
+        }
+        contador--;
+    }
+
+    public int getContador() {
+        return contador;
+    }
+}
+
+class Nodo<T> {
+    private T elemento;
+    private Nodo<T> siguiente;
+
+    public Nodo(T elemento) {
+        this.elemento = elemento;
+        this.siguiente = null;
+    }
+
+    public T getElemento() {
+        return elemento;
+    }
+
+    public void setElemento(T elemento) {
+        this.elemento = elemento;
+    }
+
+    public Nodo<T> getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo<T> siguiente) {
+        this.siguiente = siguiente;
+    }
+}
