75dd5080d7cffa70e4eaffc9e23b036d84c2ce86
Mancaceresuade <mancaceres@uade.edu.ar>
2025-04-03 10:40:58 -0300

    pila estatica

---
+++ progii/src/clase4/EjemploPilaEstatica.java
@@ -0,0 +1,57 @@
+package clase4;
+
+public class EjemploPilaEstatica {
+    public static void main(String[] args) {
+        Pila<String> pila = new Pila<>(5);
+        pila.apilar("Hola");
+        pila.apilar("Mundo");
+        pila.apilar("Java");
+        System.out.println(pila.cima());        
+        System.out.println(pila.desapilar());
+        System.out.println(pila.cima());
+    }
+}
+
+class Pila<T> {
+    private T[] elementos;
+    private int contador = 0;
+
+    public Pila(int capacidad) {
+        elementos = (T[]) new Object[capacidad];
+    }
+
+    public void apilar(T elemento) {
+        if (contador < elementos.length) {
+            elementos[contador++] = elemento;
+        } else {
+            System.out.println("Pila llena");
+        }
+    }
+
+    public T desapilar() {
+        if (contador > 0) {
+            return elementos[--contador];
+        } else {
+            System.out.println("Pila vacia");
+            return null;
+        }
+    }
+
+    public boolean esVacia() {
+        return contador == 0;
+    }
+
+    public int tamano() {
+        return contador;
+    }
+
+    public T cima() {
+        if (contador > 0) {
+            return elementos[contador - 1];
+        } else {
+            System.out.println("Pila vacia");
+            return null;
+        }
+    }
+
+}
